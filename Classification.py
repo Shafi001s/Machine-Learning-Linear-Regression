df58bafe04e1906b84e1261d69382677c54dc67a
Shafi001s <shafiahmed001s@gmail.com>
2025-11-19 21:38:21 +0000

    adeed predict  funtion

--- Classification.py
+++ Classification.py
@@ -161,7 +161,44 @@ iters = 10000
 w_out, b_out, _ = gradient_descent(x_train, y_train, w_tmp, b_tmp, alph, iters) 
 print(f"\nupdated parameters: w:{w_out}, b:{b_out}")
 
+def predict(X, w, b): 
+    """
+    Predict whether the label is 0 or 1 using learned logistic
+    regression parameters w
+    
+    Args:
+      X : (ndarray Shape (m,n)) data, m examples by n features
+      w : (ndarray Shape (n,))  values of parameters of the model      
+      b : (scalar)              value of bias parameter of the model
 
+    Returns:
+      p : (ndarray (m,)) The predictions for X using a threshold at 0.5
+    """
+    # number of training examples
+    m, n = X.shape   
+    p = np.zeros(m)
+   
+    ### START CODE HERE ### 
+    # Loop over each example
+    for i in range(m):   
+        z_wb = 0                 # initialize to zero
+        
+        # Loop over each feature
+        for j in range(n): 
+            # Add the corresponding term to z_wb
+            z_wb += X[i, j] * w[j]
+        
+        # Add bias term 
+        z_wb += b
+        
+        # Calculate the prediction for this example
+        f_wb = sigmoid(z_wb)
+
+        # Apply the threshold
+        p[i] = 1 if f_wb >= 0.5 else 0
+        
+    ### END CODE HERE ### 
+    return p
 
 
 """
